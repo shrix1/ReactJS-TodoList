986ce3e2dfae653c8c24742e7effc3516b4c3205
Shriprasanna B <shriprasanna32@gmail.com>
2022-04-11 11:11:15 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,1 +1,44 @@
+import React,{useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faAdd} from '@fortawesome/free-solid-svg-icons'
+import './App.css';
 
+export default function ToDo(){
+   const [todo,setTodo] = useState([])
+   const [task,setTask] = useState("")
+   
+    function create(ev){
+        ev.preventDefault()
+        setTodo(oldTask=>{
+           setTask("")
+           return [...oldTask,task]
+        })
+    }
+    
+    return <>
+        <div>
+            <h1>todo app</h1>
+            <form>
+                <input type="text" value={task} onChange={e=>{
+                    setTask(e.target.value)
+                }}/>
+                <button type='submit' onClick={create}>
+                    <FontAwesomeIcon icon={faAdd} />
+                </button>
+            </form> 
+        </div>
+        <main>
+           <ul>
+                {todo.map((element)=>{
+                    return <li>{element}</li>
+                })}
+           </ul>
+        </main>
+      </>
+}
+
+//value={task} get the value from input 
+//"onChange"=>{e=>{setTask(e.target.value)} use this to map the value to setTask()
+
+//use "todo" array bcz putting values into arr and calling is easy
+//by creating a btn click func to set the value into a list by using setTodo()
--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './App.css';
+import Todo from "./app"
 
-function NavBar(){
-  return(
-      <ul>
-        <li>one</li>
-        <li>two</li>
-        <li>three</li>
-      </ul>
-  )
-}
-
-ReactDOM.render(
-  <div>
-
-    <h1>hello shri wassup</h1>
-    <NavBar/> {/*Component*/}
-
-  </div>
-  ,
+ReactDOM.render
+(
+  <Todo/>,
   document.getElementById('root')
 );
-
