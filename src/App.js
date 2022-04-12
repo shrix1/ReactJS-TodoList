64eec11afc8cf0c5e5584f0b75395599470b3461
Shriprasanna B <shriprasanna32@gmail.com>
2022-04-12 20:35:02 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React,{useState} from "react";
+import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faAdd} from '@fortawesome/free-solid-svg-icons'
 import './App.css';
+import {useState ,useEffect} from "react";
 
 export default function ToDo(){
    const [todo,setTodo] = useState([])
